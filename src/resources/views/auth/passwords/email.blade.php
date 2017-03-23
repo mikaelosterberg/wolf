f1f88cda5e84d8aaa37a26b7c0d0c1c4f980af38
Mikael Osterberg <mikael@osterberg.me>
2017-03-23 19:38:48 +0100

    Redirect fix and template fixes.

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -1,22 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
+    <form class="form-signin" role="form" method="POST" action="{{ route('password.email') }}">{{ csrf_field() }}
+        <h2 class="form-signin-heading">Password Reset</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" value="{{ old('email') }}" required autofocus class="form-control" placeholder="Email address" required autofocus>
+        @if ($errors->has('email'))
+            <strong>{{ $errors->first('email') }}</strong>
+        @endif
+        <br>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Send Password Reset Link</button>
+    </form>
+@endsection
 
-@if (session('status'))
-        {{ session('status') }}
-@endif
-
-<form method="POST" action="{{ route('password.email') }}">{{ csrf_field() }}<br/>
-E-Mail Address: <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-
-@if ($errors->has('email'))
-        <strong>{{ $errors->first('email') }}</strong>
-@endif
-
-<button type="submit">
-    Send Password Reset Link
-</button>
-
-</form>
-
+@section('head')
+    <style type="text/css">
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+        .form-signin .form-signin-heading {
+            margin-bottom: 10px;
+        }
+        .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+        }
+        .form-signin .form-control:focus {
+            z-index: 2;
+        }
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+    </style>
 @endsection
--- src/resources/views/auth/passwords/reset.blade.php
+++ src/resources/views/auth/passwords/reset.blade.php
@@ -1,38 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <form class="form-signup" method="POST" action="{{ route('password.request') }}">{{ csrf_field() }}<input type="hidden" name="token" value="{{ $token }}">
 
-@if (session('status'))
-        {{ session('status') }}
-@endif
+        <h2 class="form-signup-heading">Reset Password</h2>
 
-<form method="POST" action="{{ route('password.request') }}">
-{{ csrf_field() }}
+        <label for="email" class="sr-only">Email address</label>
+        <input type="email" name="email" value="{{ old('email') }}" required autofocus class="form-control first" placeholder="Email address" required autofocus>
+        @if ($errors->has('email'))
+            <strong>{{ $errors->first('email') }}</strong>
+        @endif
 
-<input type="hidden" name="token" value="{{ $token }}">
+        <label for="password" class="sr-only">Password</label>
+        <input id="password" type="password" class="form-control input" name="password" placeholder="Password" required>
+        @if ($errors->has('password'))
+            <strong>{{ $errors->first('password') }}</strong>
+        @endif
 
-E-Mail Address: <input type="email" name="email" value="{{ $email or old('email') }}" required autofocus>
 
-@if ($errors->has('email'))
-        <strong>{{ $errors->first('email') }}</strong>
-@endif
+        <label for="password_confirmation" class="sr-only">Confirm Password</label>
+        <input id="password" type="password" class="form-control last" name="password_confirmation" placeholder="Confirm Password" required>
+        @if ($errors->has('password_confirmation'))
+            <strong>{{ $errors->first('password_confirmation') }}</strong>
+        @endif
 
-Password:  <input type="password" name="password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Reset Password</button>
 
-@if ($errors->has('password'))
-        <strong>{{ $errors->first('password') }}</strong>
-@endif
-Confirm Password:  <input type="password" name="password_confirmation" required>
+    </form>
+@endsection
 
-@if ($errors->has('password_confirmation'))
-        <strong>{{ $errors->first('password_confirmation') }}</strong>
-@endif
+@section('head')
+    <style type="text/css">
+        .form-signup {
+            max-width: 600px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+        .form-signup .form-signup-heading
+        {
+            margin-bottom: 10px;
+        }
 
-<button type="submit">
-    Reset Password
-</button>
-
-</form>
-
+        .form-signup .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+        }
+        .form-signup .form-control:focus {
+            z-index: 2;
+        }
+        .form-signup .input {
+            border-radius: 0;
+            border-radius: 0;
+        }
+        .form-signup .first {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+        .form-signup .last {
+            margin-bottom: 15px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+    </style>
 @endsection
